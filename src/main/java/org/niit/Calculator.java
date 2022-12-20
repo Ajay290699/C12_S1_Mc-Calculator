7a50a7683c26779ac6f0a613f088589fdeca065b
Ajay Chavan <ajayc9838@gmail.com>
2022-12-20 17:16:55 +0530

    changes done

---
+++ src/main/java/org/niit/Calculator.java
@@ -0,0 +1,24 @@
+package org.niit;
+
+public class Calculator {
+
+    public int addition(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    public int subtraction(int a, int b) {
+        int c = a - b;
+        return c;
+    }
+
+    public int multiplication(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+    public int division(int a, int b) {
+        int c = a / b;
+        return c;
+    }
+}
